e277c2071cbeea296ddb3f59fb86a1dd97e796ae
gasantos <gasantos@student.42sp.org.br>
2022-02-05 20:11:26 +0100

    Adicionando Rush

---
+++ rush00/bonus_rush03/rush03.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gasantos <gasantos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/04 23:50:16 by bbonaldi          #+#    #+#             */
+/*   Updated: 2022/02/05 19:51:29 by gasantos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+void	rush(int x, int y);
+
+typedef struct t_characters {
+	char	first;
+	char	second;
+	char	third;
+}	t_characters;
+
+void	ft_start_end_print(t_characters chars, int l, int columns)
+{
+	if (l == 0)
+	{
+		ft_putchar(chars.first);
+	}
+	else if (l == columns -1)
+	{
+		ft_putchar(chars.third);
+	}
+	else
+	{
+		ft_putchar(chars.second);
+	}
+}
+
+void	ft_print_row(t_characters chars, int columns)
+{
+	int	i;
+
+	i = 0;
+	while (i < columns)
+	{
+		ft_start_end_print(chars, i, columns);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+void	ft_init_struct(t_characters *chars, char first, char second, char third)
+{
+	chars->first = first;
+	chars->second = second;
+	chars->third = third;
+}
+
+void	rush(int x, int y)
+{
+	int				k;
+	t_characters	first_row;
+	t_characters	middle_row;
+	t_characters	last_row;
+
+	ft_init_struct(&first_row, 'A', 'B', 'C');
+	ft_init_struct(&middle_row, 'B', ' ', 'B');
+	ft_init_struct(&last_row, 'A', 'B', 'C');
+	if (y > 0)
+	{
+		ft_print_row(first_row, x);
+	}
+	k = 0;
+	while (k < (y -2))
+	{
+		ft_print_row(middle_row, x);
+		k++;
+	}
+	if (y > 1)
+	{
+		ft_print_row(last_row, x);
+	}
+}
