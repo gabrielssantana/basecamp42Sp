42d8c60a386f353b6821787012524d54afbe6a05
gasantos <gasantos@student.42sp.org.br>
2022-02-07 03:06:14 +0100

    mais explicação

--- rush00/bonus_arguments_rush04/main.c
+++ rush00/bonus_arguments_rush04/main.c
@@ -6,7 +6,7 @@
 /*   By: gasantos <gasantos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 02:07:43 by bbonaldi          #+#    #+#             */
-/*   Updated: 2022/02/07 01:37:04 by gasantos         ###   ########.fr       */
+/*   Updated: 2022/02/07 03:05:23 by gasantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	rush(int x, int y);
 
+// função que escreve a mensagem de erro
 void	print_warning(void)
 {
 	write(1, ("Please provide two integers "\
@@ -21,6 +22,7 @@ void	print_warning(void)
 	"first:	x	(columns)\nsecond:	y	(rows)\n"), 104);
 }
 
+//função que valida se o caractere passado é um número inteiro
 int	is_number(char c)
 {
 	if (c >= '0' && c <= '9')
@@ -30,6 +32,7 @@ int	is_number(char c)
 	return (0);
 }
 
+//converte o número de string para inteiro
 int	convert_string_argv(char *each_argv)
 {
 	int	i;
@@ -37,6 +40,7 @@ int	convert_string_argv(char *each_argv)
 
 	i = 0;
 	result = 0;
+// percorre todo o array string até encontrar \0 que sinaliza o fim do array
 	while (each_argv[i] != '\0')
 	{
 		if (is_number(each_argv[i]))
@@ -56,11 +60,12 @@ int	main(int argc, char *argv[])
 {
 	int	x;
 	int	y;
-
+//valida se foram passados 2 argumentos na linha de comando
 	if (argc == 3)
 	{
 		x = convert_string_argv(argv[1]);
 		y = convert_string_argv(argv[2]);
+//valida se o argumento passado é inválido e escreve o erro
 		if (x == 0 || y == 0)
 		{
 			print_warning();
